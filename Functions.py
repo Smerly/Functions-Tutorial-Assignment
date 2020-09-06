d1caa4a2eab292cc68bfd9fcfb9e4e3c78f66a9a
Smerly <69550699+Smerly@users.noreply.github.com>
2020-09-05 19:52:44 -0700

    Add files via upload

---
+++ Functions.py
@@ -0,0 +1,42 @@
+def area(width, height):
+    result = width * height
+    return result
+
+
+
+result = area(5, 6)
+print(result)
+
+result = area(100, 3)
+print(result)
+
+result = area(12, 12)
+print(result)
+
+def subtract(num1, num2):
+    result = num1 - num2
+    return result
+
+
+result = subtract(100, 30)
+print(result)
+
+
+
+def divide(dividend, divisor):
+    quotient = dividend / divisor
+    return quotient
+
+quotient = divide(6, 3)
+print(quotient)
+
+
+def add(num1, num2):
+    sum = num1 + num2
+    return sum
+
+sum = add(9, 10)
+print(sum)
+
+sum = add(21, 0)
+print(sum)
